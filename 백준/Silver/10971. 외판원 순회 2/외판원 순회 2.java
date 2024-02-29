d0a64422c371fc7fceca4de3218f4766133d8423
altys31 <91894012+altys31@users.noreply.github.com>
2024-02-29 15:03:42 +0900

    [Silver II] Title: 외판원 순회 2, Time: 148 ms, Memory: 15216 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,12 +7,14 @@ import java.util.StringTokenizer;
 public class Main {
 	static int N;
 	static int answer;
+	static int[][] arr;
+	static int[] num;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		N = Integer.parseInt(br.readLine());
-		int[][] arr = new int[N+1][N+1];
-		int[] num = new int[N+1];
+		arr = new int[N+1][N+1];
+		num = new int[N+1];
 		answer = Integer.MAX_VALUE;
 		for(int i = 1; i<N+1; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -22,44 +25,37 @@ public class Main {
 		for(int i = 1; i<=N; i++)
 			num[i] = i;
 		
-		perm(1,new int[N+1],num,arr);
+		perm(1,new int[N+1],0,0);
 		System.out.println(answer);
 		
 	}
 	
-	static void perm(int toselect, int[] selected,int[] num,int[][] arr) {
+	static void perm(int toselect, int[] selected, int total,int previous) {
+		
+		if(total >= answer)
+			return;
 		
 		if(toselect == N+1) {
-//			for(int i = 1; i<N+1; i++) {
-//				System.out.print(selected[i]+" ");
-//			}
-//			System.out.println();
-			
-			int total = 0;
-			int start = selected[1];
-			int previous = selected[1];
-			for(int i = 2; i<N+1; i++ ) {
-				if(arr[previous][selected[i]] != 0)
-					total += arr[previous][selected[i]];
-				else
-					return;
-				previous = selected[i];
-			}
-			
-			if(arr[previous][start] != 0)
-				total += arr[previous][start];
+			if(arr[previous][selected[1]] != 0)
+				total += arr[previous][selected[1]];
 			else
 				return;
-			
 			answer = Math.min(answer, total);
 		}
 		else {
 			for(int i = 1; i<=N; i++) {
+				if(previous != 0 && arr[previous][i] == 0)
+					continue;
+				
 				if(num[i] != 0) {
+					if(previous != 0)
+						total += arr[previous][i];
 					selected[toselect] = i;
-				num[i] = 0;
-				perm(toselect+1, selected,num,arr);
-				num[i] = i;
+					num[i] = 0;
+					perm(toselect+1, selected,total,i);
+					if(previous != 0)
+						total -= arr[previous][i];
+					num[i] = i;
 				}
 			}
 		}
