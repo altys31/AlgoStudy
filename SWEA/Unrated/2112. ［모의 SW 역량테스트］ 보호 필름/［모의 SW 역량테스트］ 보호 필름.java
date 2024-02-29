f0cac6919b1fb597fc6b67e049708706d83ac0b6
altys31 <91894012+altys31@users.noreply.github.com>
2024-02-29 11:49:50 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 418 ms, Memory: 88,292 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -12,7 +11,8 @@ public class Solution {
 	static int W;
 	static int K;
 	static int answer;
-	
+	static boolean[] allT;
+	static boolean[] allF;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -27,6 +27,11 @@ public class Solution {
 			W = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
 			answer = Integer.MAX_VALUE;
+			allT = new boolean[W];
+			allF = new boolean[W];
+			
+			Arrays.fill(allF, false);
+			Arrays.fill(allT, true);
 			
 			boolean[][] film  = new boolean[D][W];
 			
@@ -45,39 +50,39 @@ public class Solution {
 	
 	static void bt(boolean[][] film,int depth,boolean visited[],int start) {
 
+		if(depth > K || depth >= answer)
+			return;
+		
 		if(check(film)) {
 			answer = Math.min(depth, answer);
 			return;
 		}
 		
-		if(depth >= K || depth >= answer)
-			return;
+		
 		
 		boolean[] layer = new boolean[W];
 		
+		//시간초과나서 탐색 시작부분을 조절함 
 		for(int i = start; i<D; i++) {
 
 			if(!visited[i]) {
+				boolean[] original = film[i];
+				
 				//백트래킹을 위한 저장
-				for(int j = 0; j<W; j++) {
-					layer[j] = film[i][j];
-				}
 				visited[i] =true;
-				Arrays.fill(film[i], true);
+				film[i] = allT;
 				bt(film,depth+1,visited,i);
-				Arrays.fill(film[i], false);
+				film[i] = allF;
 				bt(film,depth+1,visited,i);
+				film[i] = original;
 				visited[i] =false;
 
 				//백트래킹을 위한 복구
-				for(int j = 0; j<W; j++) {
-					film[i][j] = layer[j];
-				}
 			}
 		}
 	}
 	
-	
+	//필름 체크
 	static boolean check(boolean[][] layer) {
 	
 		for(int i = 0; i<W; i++) {
@@ -96,7 +101,5 @@ public class Solution {
 				return false;
 		}
 		return true;
-		
 	}
-
 }
