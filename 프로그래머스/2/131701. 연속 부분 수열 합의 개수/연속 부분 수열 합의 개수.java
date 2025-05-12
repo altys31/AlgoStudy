dd42739f4b05c02a07fe4bc6f75d72c3578d6f00
altys31 <91894012+altys31@users.noreply.github.com>
2025-05-12 23:43:59 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1920.22 ms, Memory: 138 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[] elements) {
+        int answer = 0;
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i =0; i<elements.length; i++){
+            for(int j =0; j<elements.length; j++){
+                
+                int pointer = j;
+                int sum = 0;
+                
+                for(int k = 0; k<=i; k++){
+                   sum += elements[(pointer+k) % elements.length];
+                }
+                
+                set.add(sum);
+            }   
+        }
+        
+        answer = set.size();
+        return answer;
+    }
+}
